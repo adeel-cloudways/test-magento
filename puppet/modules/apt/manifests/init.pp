bd160e34fdfad0d1deaf5bc7d60b7a029a1bfcc7
root <root@ubuntu.example.com>
2013-01-17 08:54:58 +0000

    test magento9

--- puppet/modules/apt/manifests/init.pp
+++ puppet/modules/apt/manifests/init.pp
@@ -13,7 +13,7 @@ class apt {
         exec { 'add repo':
                 command => "add-apt-repository ppa:nginx/stable",
                 path    => "/usr/local/bin/:/bin/:/usr/bin/:",
-		require => Exec['python-software-properties'];
+		require => Package['python-software-properties'];
                 
 		'apt-get update':
                 command => "apt-get update",
